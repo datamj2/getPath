afbe8e0d50a92d8aebdcc964b31cf826d97b4a5d
datamj2 <mjansen2@gmail.com>
2017-11-13 09:00:33 -0800

    Initial Commit

---
+++ getPath/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  getPath
+//
+//  Created by mj on 11/13/17.
+//  Copyright © 2017 mj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
