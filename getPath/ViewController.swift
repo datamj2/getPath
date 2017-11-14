78b142668e324370466e5ed0556119f148f14191
datamj2 <mjansen2@gmail.com>
2017-11-13 21:58:03 -0800

    commit02

--- getPath/ViewController.swift
+++ getPath/ViewController.swift
@@ -1,15 +1,42 @@
 //
 //  ViewController.swift
-//  getPath
+//  getFilePath
 //
-//  Created by mj on 11/13/17.
+//  Created by mj on 11/12/17.
 //  Copyright © 2017 mj. All rights reserved.
 //
 
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var filename_field: NSTextField!
 
+    @IBAction func browseFile(sender: AnyObject) {
+        
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a file or folder";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+//        dialog.allowedFileTypes        = ["txt", "pdf"];
+        
+        if (dialog.runModal() == NSModalResponseOK) {
+            let result = dialog.URL // Pathname of the file
+            
+            if (result != nil) {
+                let path = result!.path!
+                filename_field.stringValue = path
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
